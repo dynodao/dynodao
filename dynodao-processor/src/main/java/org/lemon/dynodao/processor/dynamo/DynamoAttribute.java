2fb551b8afb3830cac74784083967c2e20a294bf
Taras Mychaskiw <tarasmy@amazon.com>
2019-01-16 12:05:27 -0500

    add toNewAttributeValue in DynamoAttribute

--- dynodao-processor/src/main/java/org/lemon/dynodao/processor/dynamo/DynamoAttribute.java
+++ dynodao-processor/src/main/java/org/lemon/dynodao/processor/dynamo/DynamoAttribute.java
@@ -17,16 +17,24 @@ public class DynamoAttribute {
     private final String name;
     private final VariableElement field;
 
+    /**
+     * @return this attribute as a field to be placed into a pojo class
+     */
     public FieldSpec toFieldSpec() {
         return FieldSpec.builder(TypeName.get(field.asType()), field.getSimpleName().toString())
                 .addModifiers(Modifier.PRIVATE, Modifier.FINAL)
                 .build();
     }
 
-    public CodeBlock newAttributeValue() {
-        return CodeBlock.builder()
-                .addStatement("return new $T.$L($N)", attributeValue(), "withS", toFieldSpec())
-                .build();
+    /**
+     * Returns this attribute as a new {@link com.amazonaws.services.dynamodbv2.model.AttributeValue} block
+     * of code. The field name is assumed to be the same field name as the field returned by {@link #toFieldSpec()}.
+     * The block is standalone, no semicolon or statement is included, it is similar to
+     * <tt>new AttributeValue().withS(hashKey)</tt>.
+     * @return the block of code that initializes this as an {@link com.amazonaws.services.dynamodbv2.model.AttributeValue}
+     */
+    public CodeBlock toNewAttributeValue() {
+        return CodeBlock.of("new $T.$L($N)", attributeValue(), "withS", toFieldSpec());
     }
 
 }
