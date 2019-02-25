22dc6bafbd850c396fa0483b8bdca1488589ea27
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2019-02-25 15:56:38 -0500

    interface documentation

--- dynodao-api/src/main/java/org/lemon/dynodao/DynoDaoLoad.java
+++ dynodao-api/src/main/java/org/lemon/dynodao/DynoDaoLoad.java
@@ -5,10 +5,23 @@ import com.amazonaws.services.dynamodbv2.model.GetItemRequest;
 
 import java.util.stream.Stream;
 
+/**
+ * A DynamoDB <tt>load</tt> operation, getting a single document of type <tt>T</tt>.
+ * @param <T> the type of document stored in DynamoDB
+ */
 public interface DynoDaoLoad<T> {
 
+    /**
+     * Loads and returns the single document.
+     * @param amazonDynamoDb the dynamoDb client to use
+     * @return a singleton stream document matching this instance specification, or an empty stream if none match
+     */
     Stream<T> load(AmazonDynamoDB amazonDynamoDb);
 
+    /**
+     * Returns the {@link GetItemRequest} which will be used in the load operation.
+     * @return the {@link GetItemRequest}
+     */
     GetItemRequest asRequest();
 
 }
--- dynodao-api/src/main/java/org/lemon/dynodao/DynoDaoQuery.java
+++ dynodao-api/src/main/java/org/lemon/dynodao/DynoDaoQuery.java
@@ -5,10 +5,23 @@ import com.amazonaws.services.dynamodbv2.model.QueryRequest;
 
 import java.util.stream.Stream;
 
+/**
+ * A DynamoDB <tt>query</tt> operation, getting a collection of documents of type <tt>T</tt>.
+ * @param <T> the type of document stored in DynamoDB
+ */
 public interface DynoDaoQuery<T> {
 
+    /**
+     * Queries for and returns a stream of documents.
+     * @param amazonDynamoDb the dynamoDb client to use
+     * @return a stream of documents matching this instance specification, or an empty stream if none match
+     */
     Stream<T> query(AmazonDynamoDB amazonDynamoDb);
 
+    /**
+     * Returns the {@link QueryRequest} which will be used in the query operation.
+     * @return the {@link QueryRequest}
+     */
     QueryRequest asRequest();
 
 }
