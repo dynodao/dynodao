3a987cc9386f8cd066b837d102974303746f2d90
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2019-03-02 14:43:53 -0500

    remove DynamoDBMapper related methods from DynamoDbUtil

--- dynodao-processor/src/main/java/org/lemon/dynodao/processor/util/DynamoDbUtil.java
+++ dynodao-processor/src/main/java/org/lemon/dynodao/processor/util/DynamoDbUtil.java
@@ -13,33 +13,6 @@ import javax.lang.model.element.TypeElement;
 @UtilityClass
 public class DynamoDbUtil {
 
-    private static final ClassName PAGINATED_LIST = ClassName.get("com.amazonaws.services.dynamodbv2.datamodeling", "PaginatedList");
-    private static final ClassName DYNAMO_DB_MAPPER = ClassName.get("com.amazonaws.services.dynamodbv2.datamodeling", "DynamoDBMapper");
-    private static final ClassName DYNAMO_DB_QUERY_EXPRESSION = ClassName.get("com.amazonaws.services.dynamodbv2.datamodeling", "DynamoDBQueryExpression");
-
-    /**
-     * @param document the document type
-     * @return the type name for {@link com.amazonaws.services.dynamodbv2.datamodeling.PaginatedList} with the document as the template type
-     */
-    public static ParameterizedTypeName paginatedList(TypeElement document) {
-        return ParameterizedTypeName.get(PAGINATED_LIST, TypeName.get(document.asType()));
-    }
-
-    /**
-     * @return the {@link com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper} type
-     */
-    public static ClassName dynamoDbMapper() {
-        return DYNAMO_DB_MAPPER;
-    }
-
-    /**
-     * @param document the document type
-     * @return the type name for {@link com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression} with the document as the template type
-     */
-    public static ParameterizedTypeName dynamoDbQueryExpression(TypeElement document) {
-        return ParameterizedTypeName.get(DYNAMO_DB_QUERY_EXPRESSION, TypeName.get(document.asType()));
-    }
-
     /**
      * @return the {@link com.amazonaws.services.dynamodbv2.model.AttributeValue} type
      */
