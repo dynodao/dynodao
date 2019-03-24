10176d1d8560e6be485ce540dabe78355a309f70
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2019-03-24 11:13:36 -0400

    Update ParameterizedTestSources.java

--- dynodao-processor/src/test/java/org/dynodao/processor/test/ParameterizedTestSources.java
+++ dynodao-processor/src/test/java/org/dynodao/processor/test/ParameterizedTestSources.java
@@ -13,9 +13,9 @@ public class ParameterizedTestSources {
 
     public static final String TOTAL_SEGMENTS_METHOD_SOURCE = "org.dynodao.processor.test.ParameterizedTestSources#totalSegmentsSource";
 
-    private static final int[] TOTAL_SEGMENTS = IntStream.rangeClosed(
-            Runtime.getRuntime().availableProcessors() - 1, Runtime.getRuntime().availableProcessors() + 1)
-            .toArray();
+    private static final int MIN_SEGMENTS = Math.max(2, Runtime.getRuntime().availableProcessors() - 1);
+    private static final int MAX_SEGMENTS = Math.min(MIN_SEGMENTS + 2, Runtime.getRuntime().availableProcessors() + 1);
+    private static final int[] TOTAL_SEGMENTS = IntStream.rangeClosed(MIN_SEGMENTS, MAX_SEGMENTS).toArray();
 
     /**
      * Returns the parameters for the number of total segments in parallel scan tests.
