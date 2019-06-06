b220e1493136fea0751b6fb7f3ac279e0b2fe8d3
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2019-06-06 19:32:56 -0400

    test documents

--- dynodao-processor/src/test/java/org/dynodao/processor/itest/serialization/document/DocumentSerializationTest.java
+++ dynodao-processor/src/test/java/org/dynodao/processor/itest/serialization/document/DocumentSerializationTest.java
@@ -1,17 +1,26 @@
 package org.dynodao.processor.itest.serialization.document;
 
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import org.dynodao.processor.itest.AbstractSourceCompilingTest;
+import org.dynodao.processor.itest.AbstractIntegrationTest;
+import org.dynodao.processor.test.ParameterizedTestSources;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullSource;
 
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.stream.Stream;
 
 import static java.util.Collections.emptyMap;
 import static java.util.Collections.singletonMap;
 import static org.assertj.core.api.Assertions.assertThat;
 
-class DocumentSerializationTest extends AbstractSourceCompilingTest {
+class DocumentSerializationTest extends AbstractIntegrationTest {
+
+    private static final String TABLE = "things";
+    private static final String HASH_KEY_VALUE = "hashKey";
 
     @Test
     void serializeDocument_null_returnsNullAttributeValue() {
@@ -19,22 +28,16 @@ class DocumentSerializationTest extends AbstractSourceCompilingTest {
         assertThat(value).isEqualTo(new AttributeValue().withNULL(true));
     }
 
-    @Test
-    void serializeDocument_allFieldsPresent_returnsMapAttributeValueWithOverrideAttributeNames() {
-        AttributeValue value = SchemaAttributeValueSerializer.serializeDocument(document("a1", "a2", "a3"));
-        assertThat(value).isEqualTo(attributeValue("a1", "a2", "a3"));
-    }
-
-    @Test
-    void serializeDocument_someFieldsNull_returnsMapAttributeValueWithNulls() {
-        AttributeValue value = SchemaAttributeValueSerializer.serializeDocument(document(null, null, "a3"));
-        assertThat(value).isEqualTo(attributeValue(null, null, "a3"));
-    }
-
-    @Test
-    void serializeDocument_allFieldsNull_returnsMapAttributeValueWithNulls() {
-        AttributeValue value = SchemaAttributeValueSerializer.serializeDocument(document(null, null, null));
-        assertThat(value).isEqualTo(attributeValue(null, null, null));
+    @ParameterizedTest
+    @CsvSource({
+            "a1,a2,a3",
+            "a1,a2,", "a1,,a3", ",a2,a3",
+            "a1,,", ",a2,", ",,a3",
+            ",,"
+    })
+    void serializeDocument_typicalUseCases_returnsMapAttributeWithCorrectAttributeNames(String attribute1, String attribute2, String attribute3) {
+        AttributeValue value = SchemaAttributeValueSerializer.serializeDocument(document(attribute1, attribute2, attribute3));
+        assertThat(value).isEqualTo(attributeValue(attribute1, attribute2, attribute3));
     }
 
     @Test
@@ -43,82 +46,78 @@ class DocumentSerializationTest extends AbstractSourceCompilingTest {
         assertThat(item).isEmpty();
     }
 
-    @Test
-    void serializeDocumentAsItem_allFieldsPresent_returnsMapWithOverrideAttributeNames() {
-        Map<String, AttributeValue> item = SchemaAttributeValueSerializer.serializeDocumentAsItem(document("a1", "a2", "a3"));
-        assertThat(item).isEqualTo(item("a1", "a2", "a3"));
+    @ParameterizedTest
+    @CsvSource({
+            "a1,a2,a3",
+            "a1,a2,", "a1,,a3", ",a2,a3",
+            "a1,,", ",a2,", ",,a3",
+            ",,"
+    })
+    void serializeDocumentAsItem_typicalUseCases_returnsMap(String attribute1, String attribute2, String attribute3) {
+        Map<String, AttributeValue> item = SchemaAttributeValueSerializer.serializeDocumentAsItem(document(attribute1, attribute2, attribute3));
+        assertThat(item).isEqualTo(item(attribute1, attribute2, attribute3));
     }
 
-    @Test
-    void serializeDocumentAsItem_someFieldsNull_returnsMapWithNulls() {
-        Map<String, AttributeValue> item = SchemaAttributeValueSerializer.serializeDocumentAsItem(document(null, null, "a3"));
-        assertThat(item).isEqualTo(item(null, null, "a3"));
-    }
-
-    @Test
-    void serializeDocumentAsItem_allFieldsNull_returnsMapWithNulls() {
-        Map<String, AttributeValue> item = SchemaAttributeValueSerializer.serializeDocumentAsItem(document(null, null, null));
-        assertThat(item).isEqualTo(item(null, null, null));
-    }
-
-    @Test
-    void deserializeDocument_null_returnsNull() {
-        Document value = SchemaAttributeValueSerializer.deserializeDocument(null);
+    @ParameterizedTest
+    @NullSource
+    @MethodSource(ParameterizedTestSources.ATTRIBUTE_VALUES_WITHOUT_MAP_SOURCE)
+    void deserializeDocument_nullCases_returnsNull(AttributeValue attributeValue) {
+        Document value = SchemaAttributeValueSerializer.deserializeDocument(attributeValue);
         assertThat(value).isNull();
     }
 
-    @Test
-    void deserializeDocument_nullAttributeValue_returnsNull() {
-        Document value = SchemaAttributeValueSerializer.deserializeDocument(new AttributeValue().withNULL(true));
-        assertThat(value).isNull();
-    }
-
-    @Test
-    void deserializeDocument_mapValueNull_returnsNull() {
-        Document value = SchemaAttributeValueSerializer.deserializeDocument(new AttributeValue().withS("not map"));
-        assertThat(value).isNull();
-    }
-
-    @Test
-    void deserializeDocument_mapAttributeValue_returnsDocument() {
-        Document value = SchemaAttributeValueSerializer.deserializeDocument(attributeValue("a1", "a2", "a3"));
-        assertThat(value).isEqualTo(document("a1", "a2", "a3"));
+    @ParameterizedTest
+    @CsvSource({
+            "a1,a2,a3",
+            "a1,a2,", "a1,,a3", ",a2,a3",
+            "a1,,", ",a2,", ",,a3",
+            ",,"
+    })
+    void deserializeDocument_correctKeysAndTypesMapCases_returnsDocument(String attribute1, String attribute2, String attribute3) {
+        Document value = SchemaAttributeValueSerializer.deserializeDocument(attributeValue(attribute1, attribute2, attribute3));
+        assertThat(value).isEqualTo(document(attribute1, attribute2, attribute3));
     }
 
     @Test
     void deserializeDocument_emptyMap_returnsDocumentWithNullFields() {
-        Document value = SchemaAttributeValueSerializer.deserializeDocument(new AttributeValue().withM(emptyMap()));
+        Document document = SchemaAttributeValueSerializer.deserializeDocument(new AttributeValue().withM(emptyMap()));
+        assertThat(document).isEqualTo(document(null, null, null));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "dynamoNameIsAttribute3,value",
+            "key,value"
+    })
+    void deserializeDocument_incorrectKeys_returnsDocumentWithNullFields(String key, String value) {
+        Document document = SchemaAttributeValueSerializer.deserializeDocument(new AttributeValue().withM(singletonMap(key, new AttributeValue(value))));
+        assertThat(document).isEqualTo(document(null, null, null));
+    }
+
+    @ParameterizedTest
+    @MethodSource(ParameterizedTestSources.ATTRIBUTE_VALUES_WITHOUT_STRING_SOURCE)
+    void deserializeDocument_keysHaveWrongTypes_returnsDocumentWithNullFields(AttributeValue attributeValue) {
+        Document value = SchemaAttributeValueSerializer.deserializeDocument(new AttributeValue().withM(singletonMap("attribute1", attributeValue)));
         assertThat(value).isEqualTo(document(null, null, null));
     }
 
-    @Test
-    void deserializeDocument_valueHaveWrongKeys_returnsDocumentWithNullFields() {
-        Document value = SchemaAttributeValueSerializer.deserializeDocument(new AttributeValue().withM(singletonMap("dynamoNameIsAttribute3", new AttributeValue("a3"))));
-        assertThat(value).isEqualTo(document(null, null, null));
-    }
-
-    @Test
-    void deserializeDocument_someKeysCorrect_returnsDocumentWithNullForMissingFieldsOnly() {
-        Document value = SchemaAttributeValueSerializer.deserializeDocument(new AttributeValue().withM(singletonMap("attribute3", new AttributeValue("a3"))));
-        assertThat(value).isEqualTo(document(null, null, "a3"));
-    }
-
-    @Test
-    void deserializeDocument_keysHaveWrongTypes_returnsDocumentWithNullFields() {
-        Document value = SchemaAttributeValueSerializer.deserializeDocument(new AttributeValue().withM(singletonMap("attribute3", new AttributeValue().withM(emptyMap()))));
-        assertThat(value).isEqualTo(document(null, null, null));
-    }
-
-    @Test
-    void deserializeDocumentFromItem_null_returnsNull() {
-        Document value = SchemaAttributeValueSerializer.deserializeDocumentFromItem(null);
+    @ParameterizedTest
+    @NullSource
+    void deserializeDocumentFromItem_null_returnsNull(Map<String, AttributeValue> item) {
+        Document value = SchemaAttributeValueSerializer.deserializeDocumentFromItem(item);
         assertThat(value).isNull();
     }
 
-    @Test
-    void deserializeDocumentFromItem_allFieldsPresent_returnsDocument() {
-        Document value = SchemaAttributeValueSerializer.deserializeDocumentFromItem(item("a1", "a2", "a3"));
-        assertThat(value).isEqualTo(document("a1", "a2", "a3"));
+    @ParameterizedTest
+    @CsvSource({
+            "a1,a2,a3",
+            "a1,a2,", "a1,,a3", ",a2,a3",
+            "a1,,", ",a2,", ",,a3",
+            ",,"
+    })
+    void deserializeDocumentFromItem_correctKeysAndTypesMapCases_returnsDocument(String attribute1, String attribute2, String attribute3) {
+        Document value = SchemaAttributeValueSerializer.deserializeDocumentFromItem(item(attribute1, attribute2, attribute3));
+        assertThat(value).isEqualTo(document(attribute1, attribute2, attribute3));
     }
 
     @Test
@@ -127,22 +126,40 @@ class DocumentSerializationTest extends AbstractSourceCompilingTest {
         assertThat(value).isEqualTo(document(null, null, null));
     }
 
-    @Test
-    void deserializeDocumentFromItem_valueHaveWrongKeys_returnsDocumentWithNullFields() {
-        Document value = SchemaAttributeValueSerializer.deserializeDocumentFromItem(singletonMap("dynamoNameIsAttribute3", new AttributeValue("a3")));
+    @ParameterizedTest
+    @CsvSource({
+            "dynamoNameIsAttribute3,value",
+            "key,value"
+    })
+    void deserializeDocumentFromItem_incorrectKeys_returnsDocumentWithNullFields(String key, String value) {
+        Document document = SchemaAttributeValueSerializer.deserializeDocumentFromItem(singletonMap(key, new AttributeValue(value)));
+        assertThat(document).isEqualTo(document(null, null, null));
+    }
+
+    @ParameterizedTest
+    @MethodSource(ParameterizedTestSources.ATTRIBUTE_VALUES_WITHOUT_STRING_SOURCE)
+    void deserializeDocumentFromItem_keysHaveWrongTypes_returnsDocumentWithNullFields(AttributeValue attributeValue) {
+        Document value = SchemaAttributeValueSerializer.deserializeDocumentFromItem(singletonMap("attribute1", attributeValue));
         assertThat(value).isEqualTo(document(null, null, null));
     }
 
-    @Test
-    void deserializeDocumentFromItem_someKeysCorrect_returnsDocumentWithNullForMissingFieldsOnly() {
-        Document value = SchemaAttributeValueSerializer.deserializeDocumentFromItem(singletonMap("attribute3", new AttributeValue("a3")));
-        assertThat(value).isEqualTo(document(null, null, "a3"));
-    }
-
-    @Test
-    void deserializeDocumentFromItem_keysHaveWrongTypes_returnsDocumentWithNullFields() {
-        Document value = SchemaAttributeValueSerializer.deserializeDocumentFromItem(singletonMap("attribute3", new AttributeValue().withM(emptyMap())));
-        assertThat(value).isEqualTo(document(null, null, null));
+    @ParameterizedTest
+    @CsvSource({
+            "a1,a2,a3",
+            "a1,a2,", "a1,,a3", ",a2,a3",
+            "a1,,", ",a2,", ",,a3",
+            ",,"
+    })
+    void putAndGet_symmetricCases_returnsItem(String attribute1, String attribute2, String attribute3) {
+        Document document = document(attribute1, attribute2, attribute3);
+        Schema schema = new Schema();
+        schema.setHashKey(HASH_KEY_VALUE);
+        schema.setDocument(document);
+        put(schema);
+        Stream<Schema> items = dynoDao.get(new SchemaStagedDynamoBuilder()
+                .usingTable()
+                .withHashKey(HASH_KEY_VALUE));
+        assertThat(items).containsExactly(schema);
     }
 
     private Document document(String attribute1, String attribute2, String attribute3) {
@@ -163,6 +180,10 @@ class DocumentSerializationTest extends AbstractSourceCompilingTest {
 
     private AttributeValue attributeValue(String attribute1, String attribute2, String attribute3) {
         return new AttributeValue().withM(item(attribute1, attribute2, attribute3));
+    }
+
+    private void put(Schema item) {
+        amazonDynamoDb.putItem(TABLE, SchemaAttributeValueSerializer.serializeSchemaAsItem(item));
     }
 
 }
